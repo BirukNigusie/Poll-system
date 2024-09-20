71cd555081149c3f6861e882813dfff1ae20fbe7
BirukNigusie98 <biruknigusie98@gmail.com>
2024-09-20 22:01:44 +0300

    Inital commit

--- script.js
+++ script.js
@@ -1,18 +1,44 @@
-function countVowels(){
-    let text = document.getElementById("inputText").value;
-    let vowelCount = 0;
-    text = text.toLowerCase();
-    for (let i = 0; i < text.length; i++) {
-let char = text.charAt(i);
-if(isVowel(char)){
-    vowelCount++;
+const options = [
+    {id:'option1', text:'Javascript', votes:0},
+    {id:'option2', text:'Python', votes:0},
+    {id:'option3', text:'Java', votes:0},
+    {id:'option4', text:'C++', votes:0}
+];
+
+function submitVote(){
+    const selectedOption = document.querySelector('input[name=poll]:checked');
+    if(!selectedOption){
+        alert('Please select an option');
+        return;
+    }
+    const optionId = selectedOption.value;
+    const selectedOptionObj = options.find((option) => option.id === optionId);
+    if(selectedOptionObj){
+        selectedOptionObj.votes++;
+        console.log(selectedOptionObj);
+        displayResult();
+    }
 }
 
-    }
-    const result = document.getElementById("result");
-    result.textContent = `Total Vowels: ${vowelCount}`
+function displayResult(){
+    const result = document.getElementById('result');
+    result.innerHTML = "";
+    options.forEach((option) => {
+        const percentage = ((option.votes/getTotalVotes())*100).toFixed(2) || 0;
+        const barWidth = percentage > 0 ? percentage + "%" : "0%";
+        const optionResult = document.createElement("div");
+        optionResult.className = "option-result";
+        optionResult.innerHTML = `
+            <span class="option-text">${option.text}</span>
+            <div class="bar-container">
+                <div class="bar" style="width:${barWidth};"></div>
+                <span class="percentage">${percentage}%</span>
+            </div>
+        `;
+        result.appendChild(optionResult);
+    });
 }
-function isVowel(char){
-const vowels = ['a', 'e', 'i', 'o', 'u'];  
-return vowels.includes(char) 
+
+function getTotalVotes(){
+    return options.reduce((total, option) => total + option.votes, 0);
 }
